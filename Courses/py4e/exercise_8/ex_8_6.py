9d6847975ac7f8d2665b8753c1c3da460e4d8bd2
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-02-02 18:04:16 +0530

    update code to be better

--- Courses/py4e/exercise_8/ex_8_6.py
+++ Courses/py4e/exercise_8/ex_8_6.py
@@ -1,8 +1,13 @@
 li = []
 while True:
-    inp = input("Enter a number: ")
-    if inp == 'done':
+    inp1 = input("Enter a number: ")
+    if inp1 == 'done':
         break
+    try:
+        inp = int(inp1)
+    except:
+        print("Not a number")
+        continue
     li.append(inp)
 print("Maxinmum:",max(li))
 print("Minimum:",min(li))
