cf703874a9a004d3331ccd23648f2eb1ee3d9331
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-03-30 01:41:52 +0530

    Variables in kotlin

---
+++ Courses/kotlin/Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var userName: String = "Alex"
+    userName = "Jimmy"
+
+    val usercase: String = "val Doesn't allow changing the variable once assigned"
+
+    var age: Int = 22
+    age = 25
+
+    var newName = "Kotlin doesn't require you to define the type."
+
+    
+
+
+    println("Hello $userName! your age is $age")
+}
