1358f84bb3ea7a52c5200cb6c5809ce366e2fc73
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-03-30 14:29:19 +0530

    Char and Boolean

---
+++ Courses/kotlin/LearnKotlinProgramming/CharAndBoolean/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val myCharValue: Char = 'D'
+    val myBooleanValue: Boolean = false
+
+
+    println("myCharValue = $myCharValue and myBooleanValue = $myBooleanValue")
+}
