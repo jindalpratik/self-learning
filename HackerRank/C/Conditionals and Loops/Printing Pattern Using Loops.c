9f40fbe197a05e7a53d5af04347b811c40b7dd29
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-04-05 01:02:04 +0530

    hackerank complete c conditionals and loops

--- HackerRank/C/Conditionals and Loops/Printing Pattern Using Loops.c
+++ HackerRank/C/Conditionals and Loops/Printing Pattern Using Loops.c
@@ -6,11 +6,53 @@
 int main() 
 {
 
-    int n;
+    int n, k;
     scanf("%d", &n);
-  	for(int i = 0;i<=n;i++)
-    {
-        
+  	for(int i = 0;i <n; i++) { 
+        k = n;
+        for(int j = 0; j < n; j ++) { 
+            if(k > n-i) { 
+                printf("%d ",k);
+                k--;
+            }
+            else { 
+                printf("%d ",k);
+            }
+        }
+
+        for(int j = 0; j < n - 1; j++) {
+            if(k <= j+1) {
+                k++;
+                printf("%d ", k);
+            }
+            else {
+                printf("%d ", k);
+            }
+        }
+        printf("\n");
+    }
+  	for(int i = 0;i <n - 1; i++) { 
+        k = n;
+        for(int j = 0; j < n; j ++) { 
+            if(k > i + 2) { 
+                printf("%d ",k);
+                k--;
+            }
+            else { 
+                printf("%d ",k);
+            }
+        }
+
+        for(int j = 0; j < n - 1; j++) {
+            if(k <= j+1) {
+                k++;
+                printf("%d ", k);
+            }
+            else {
+                printf("%d ", k);
+            }
+        }
+        printf("\n");
     }
     return 0;
 }
