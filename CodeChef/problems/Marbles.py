e46ab47e1e6db560bd4e63e80758cccf019c465d
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-25 23:59:54 +0530

    Fix a typo

--- CodeChef/problems/Marbles.py
+++ CodeChef/problems/Marbles.py
@@ -9,7 +9,7 @@ for i in range(t):
     # As we have to atleast one type of marble total number of marbles we have to devide goes down to n-k 
     # Also as the marbles can be in whatever order this becomes a case of combination with repetition
     s = n - k
-    # The number of ways of making s selections from among r distinguishable possibilities, where the order does not matter and repetitions are allowed is (k+s-1)!/((k-1)!*(s)!) which is equal to (n-1)!/(k-1)! in this case as s is equal to n - k.
+    # The number of ways of making s selections from among r distinguishable possibilities, where the order does not matter and repetitions are allowed is (k+s-1)!/((k-1)!*(s)!) which is equal to (n-1)!/(k-1)!(n-k)! or (n-1)c(k-1) in this case as s is equal to n - k.
     # More information at https://math.stackexchange.com/questions/58753/distinct-ways-to-keep-n-balls-into-k-boxes , https://en.wikipedia.org/wiki/Stars_and_bars_(combinatorics)
     
     print(math.comb(n-1,k-1))
