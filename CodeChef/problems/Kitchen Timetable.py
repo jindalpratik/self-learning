93d53904c52a7995f27ab6c288fe7a9452468e56
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-27 19:59:20 +0530

    Fix file not having anything in it

---
+++ CodeChef/problems/Kitchen Timetable.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    n = int(input())
+    a_arr = [int(x) for x in input().split()]
+    b_arr = [int(x) for x in input().split()]
+    a_arr.insert(0,0)
+    count = 0
+    for k in range(n):
+        if (a_arr[k+1]-a_arr[k]) >= b_arr[k]:
+            count+=1
+    print(count)
