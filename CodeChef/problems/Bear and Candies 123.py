7929163235887394c02c91232667e88ad512f2f1
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-27 19:38:16 +0530

    Codechef Bear and Candies 123

---
+++ CodeChef/problems/Bear and Candies 123.py
@@ -0,0 +1,26 @@
+for i in range(int(input())):
+    [a,b] = [int(x) for x in input().split()]
+    tot_candies = 0
+    limak_candies = 0
+    bob_candies = 0
+    turn_l = True
+    while True:
+        tot_candies += 1
+        if turn_l:
+            temp = limak_candies + tot_candies
+            if temp <= a:
+                limak_candies = temp
+            else:
+                break
+            turn_l = False
+        else:
+            temp = bob_candies + tot_candies
+            if temp <= b:
+                bob_candies = temp
+            else:
+                break
+            turn_l = True
+    if limak_candies < bob_candies:
+        print('Bob')
+    else:
+        print('Limak')
