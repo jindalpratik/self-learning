3a02eb589b5ee3e0f525b5c1a03e5d12149c4329
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-28 20:42:13 +0530

    More codechef

---
+++ CodeChef/problems/Game between friends.py
@@ -0,0 +1,14 @@
+for i in range(int(input())):
+    [a,b,c,d] = [int(x) for x in input().split()]
+    if (b>a):
+        a += c
+    else:
+        b += c
+    if (b>a):
+        a += d
+    else:
+        b += d
+    if (b>a):
+        print('S')
+    else:
+        print('N')
