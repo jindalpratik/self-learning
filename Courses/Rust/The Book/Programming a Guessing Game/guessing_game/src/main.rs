f00422d3436d2ed415b8b7c2787cc5aeca24f4cc
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-05-28 19:40:48 +0530

    Rust, The book, Programming a Guessing Game, Complete

--- Courses/Rust/The Book/Programming a Guessing Game/guessing_game/src/main.rs
+++ Courses/Rust/The Book/Programming a Guessing Game/guessing_game/src/main.rs
@@ -1,20 +1,39 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // --snip--
 
-    println!("Please input your guess.");
+    loop {
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        println!("You guessed: {guess}");
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        // --snip--
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
