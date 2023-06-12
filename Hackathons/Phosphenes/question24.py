a59298bf12a9eff00c32be02ecaa552eeb604442
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-06-12 16:23:07 +0530

    Fix some python code.

--- Hackathons/Phosphenes/question24.py
+++ Hackathons/Phosphenes/question24.py
@@ -1,12 +1,12 @@
-arr = [6,-6]
+arr = [6, -6]
 # arr = [7,-7]
 # arr = [10,2,-5]
-for i in range((len(arr)-1),0,-1):
+for i in range((len(arr)-1), 0, -1):
     if arr[i] < 0 and arr[i-1] > 0:
         if arr[i] > (arr[i - 1]*-1):
             arr.pop(i)
         elif arr[i] < (arr[i - 1]*-1):
             arr.pop(i-1)
         else:
-            arr.pop(i), arr.pop(i-1)
+            arr.pop(i)
 print(arr)
