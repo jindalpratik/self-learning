45b2a94a9eb1e394bc82aa979aa48a08da59ae47
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-02-08 16:13:27 +0530

    NoBlackBoxMachineLearning : Drawing App: Continued

--- Courses/MachineLearning/NoBlackBoxMachineLearning/web/js/sketchPad.js
+++ Courses/MachineLearning/NoBlackBoxMachineLearning/web/js/sketchPad.js
@@ -9,12 +9,24 @@ class SketchPad {
         `;
         container.appendChild(this.canvas);
 
+        const lineBreak=document.createElement("br");
+        container.appendChild(lineBreak);
+
+        this.undoBtn=document.createElement("button");
+        this.undoBtn.innerHTML="UNDO";
+        container.appendChild(this.undoBtn);
+
         this.ctx = this.canvas.getContext("2d");
 
-        this.paths = [];
-        this.isDrawing = false;
+        this.reset();
 
         this.#addEventListeners();
+    }
+
+    reset(){
+        this.paths = [];
+        this.isDrawing = false;
+        this.#redraw();
     }
 
     #addEventListeners() {
@@ -34,11 +46,31 @@ class SketchPad {
         this.canvas.onmouseup = () => {
             this.isDrawing = false;
         };
+        this.canvas.ontouchstart=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmousedown(loc);
+        };
+        this.canvas.ontouchmove=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmousemove(loc);
+        };
+        this.canvas.ontouchend=()=>{
+            this.canvas.onmouseup();
+        };
+        this.undoBtn.onclick=()=>{
+            this.paths.pop();
+            this.#redraw();
+        }
     }
 
     #redraw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         draw.paths(this.ctx, this.paths);
+        if(this.paths.length>0){
+            this.undoBtn.disabled=false;
+        }else{
+            this.undoBtn.disabled=true;
+        }
     }
 
     #getMouse = (evt) => {
