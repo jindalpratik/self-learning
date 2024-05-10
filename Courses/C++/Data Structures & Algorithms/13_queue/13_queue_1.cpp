ebb266b5de7f24b3c74caa75e809a98046b1f6cc
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-05-10 23:40:03 +0530

    c++ stack & c++ queue

---
+++ Courses/C++/Data Structures & Algorithms/13_queue/13_queue_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(3);
+    q.push(2);
+    q.push(5);
+    cout << q.front(); // 3
+    q.pop();
+    cout << q.front(); // 2
+}
