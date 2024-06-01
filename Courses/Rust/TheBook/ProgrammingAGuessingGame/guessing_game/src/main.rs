d250864ac436de288fc58e4bf8242a447580fbfc
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-06-02 02:41:36 +0530

    Rust:TheBook:Getting Started and Programming a Guessing Game

--- Courses/Rust/TheBook/ProgrammingAGuessingGame/guessing_game/src/main.rs
+++ Courses/Rust/TheBook/ProgrammingAGuessingGame/guessing_game/src/main.rs
@@ -7,25 +7,21 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // --snip--
-
     loop {
+        println!("Please input your guess.");
+
         let mut guess = String::new();
 
-        println!("You guessed: {guess}");
-    
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("Please input your guess.");
-
-        // --snip--
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
