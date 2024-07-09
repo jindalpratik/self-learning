35249cb5d9f343a00f4669a4cd4f89dc76d79db6
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-07-10 02:25:20 +0530

    leetcode:1509_minimum_difference_between_largest_and_smallest_in_three_moves

---
+++ CompetitiveProgramming/LeetCode/problems/1509_minimum_difference_between_largest_and_smallest_in_three_moves/src/lib.rs
@@ -0,0 +1,28 @@
+use std::cmp::min;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn min_difference(mut nums: Vec<i32>) -> i32 {
+    let len = nums.len();
+    if len <= 4 {
+        return 0;
+    }
+    nums.sort();
+    let mut ans = i32::MAX;
+    for k in 0..4 {
+        ans = min(ans, nums[len - 4 + k] - nums[k]);
+    }
+    ans
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn min_difference_test() {
+        assert_eq!(min_difference(vec![1, 5, 0, 10, 14]), 1);
+    }
+}
