706e1d7b29485d75b12ae20094603d243e73958d
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-08-05 18:18:09 +0530

    Rust:TheBook:ObjectOrientedProgrammingFeaturesOfRust:Using Trait Objects That Allow for Values of Different Types

---
+++ Courses/Rust/TheBook/ObjectOrientedProgrammingFeaturesofRust/using_trait_objects_that_allow_for_vallues_of_different_types/src/main.rs
@@ -0,0 +1,34 @@
+use using_trait_objects_that_allow_for_vallues_of_different_types::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    heigth: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {}
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                heigth: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
