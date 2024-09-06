e4af9edffd509277e109ae78a077c212c2eb3d5d
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-09-06 15:27:30 +0530

    Rust:TheBook:AdvancedFeatures:Advanced Traits

---
+++ Courses/Rust/TheBook/AdvancedFeatures/unsafe_rust/src/main.rs
@@ -0,0 +1,59 @@
+use std::slice;
+
+static mut COUNTER: u32 = 0;
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+fn main() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = split_at_mut(r, 3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    unsafe {
+        println!("The value of -3 as defined in C: {}", abs(-3));
+    }
+
+    add_to_count(3);
+
+    unsafe {
+        println!("COUNTER: {COUNTER}");
+    }
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
