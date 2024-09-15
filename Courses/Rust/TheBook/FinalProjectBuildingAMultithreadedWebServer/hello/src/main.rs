ac9a352c318eaeaedbd22d26e736685965ea31d4
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-09-15 22:17:15 +0530

    Rust:TheBook:FinalProjectBuildingAMultithreadedWebServer:Graceful Shutdown and Cleanup

--- Courses/Rust/TheBook/FinalProjectBuildingAMultithreadedWebServer/hello/src/main.rs
+++ Courses/Rust/TheBook/FinalProjectBuildingAMultithreadedWebServer/hello/src/main.rs
@@ -1,29 +1,30 @@
+use hello::ThreadPool;
 use std::{
     fs,
-    io::{BufRead, BufReader, Write},
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
 };
 
-use hello::ThreadPool;
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line: String = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
@@ -37,6 +38,8 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
     stream.write_all(response.as_bytes()).unwrap();
 }
