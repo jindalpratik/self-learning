84d74e3e6b9c5e8c4d3e40e6bbdfa7dbfef8ca26
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-12-13 16:39:41 +0530

    Leetcode:Rust:217_contains_duplicate

---
+++ CompetitiveProgramming/LeetCode/problems/rust/217_contains_duplicate/src/lib.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut set = HashSet::new();
+    for i in nums {
+        if set.contains(&i) {
+            return true;
+        }
+        set.insert(i);
+    }
+    false
+}
+
+pub fn contains_duplicate_alternative(nums: Vec<i32>) -> bool {
+    let mut set = HashSet::new();
+    for i in &nums {
+        set.insert(i);
+    }
+    nums.len() != set.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn contains_duplicate_test() {
+        let vec = vec![1,2,3,1];
+        assert_eq!(contains_duplicate(vec), true);
+    }
+}
