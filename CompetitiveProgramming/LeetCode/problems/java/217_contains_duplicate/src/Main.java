fe6b657115afd8ec1329c6f6cd05b19d7d3aae18
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-12-13 16:40:26 +0530

    Leetcode:Java:217_contains_duplicate

---
+++ CompetitiveProgramming/LeetCode/problems/java/217_contains_duplicate/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(arr));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> st = new HashSet<>();
+        for (int num : nums) {
+            st.add(num);
+        }
+        return nums.length != st.size();
+    }
+
+    public static boolean containsDuplicateAlternative(int[] nums) {
+        HashSet<Integer> st = new HashSet<>();
+        for (int i = 0; i < nums.length ; i++) {
+            if(st.contains(nums[i])) {
+                return true;
+            }
+            st.add(nums[i]);
+        }
+        return false;
+    }
+}
