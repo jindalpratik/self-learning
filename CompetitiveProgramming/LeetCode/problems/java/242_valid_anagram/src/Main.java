b9a1d56a3bd27abf04b1373baa61bbac97a5a2b3
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-12-14 14:05:29 +0530

    Leetcode:Java:242_valid_anagram

---
+++ CompetitiveProgramming/LeetCode/problems/java/242_valid_anagram/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch: s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for(char ch: t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
+        }
+
+        for(int i: map.values()) {
+            if(i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
