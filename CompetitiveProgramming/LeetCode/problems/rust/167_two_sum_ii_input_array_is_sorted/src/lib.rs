00d9d741d23cf1ba3f06f3017195f6f25f90984e
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-12-18 17:06:07 +0530

    Leetcode:Rust:167_two_sum_ii_input_array_is_sorted

---
+++ CompetitiveProgramming/LeetCode/problems/rust/167_two_sum_ii_input_array_is_sorted/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut i = 0;
+    let mut j = numbers.len() - 1;
+    loop {
+        if numbers[i] + numbers[j] == target {
+            return vec![i as i32 + 1, j as i32 + 1];
+        } else if numbers[i] + numbers[j] > target {
+            j -= 1;
+        } else {
+            i += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn two_sum_test() {
+        let numbers = vec![2, 7, 11, 15];
+        let target = 9;
+        assert_eq!(two_sum(numbers, target), vec![1, 2]);
+    }
+}
