b14e9236e62425f5b57c0314ff208c43667d1486
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-12-30 19:42:36 +0530

    Leetcode:Java:20_valid_parentheses

---
+++ CompetitiveProgramming/LeetCode/problems/java/20_valid_parentheses/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "((";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch: s.toCharArray()) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else if(ch == ')') {
+                if(!st.empty() && st.peek() == '(') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else if(ch == '}') {
+                if (!st.empty() && st.peek() == '{') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else if(ch == ']') {
+                if (!st.empty() && st.peek() == '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+}
