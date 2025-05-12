ff3990fc7a04b2e0ce3429b7e0c33e94296371de
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2025-05-12 19:57:14 +0530

    Java:SpringBoot:DanVega:crash_course_using_spring_boot_3:initialize

---
+++ Courses/Java/Spring/dan_vega_crash_course/src/test/java/com/example/dan_vega_crash_course/DanVegaCrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dan_vega_crash_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanVegaCrashCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
