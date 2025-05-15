5a58696a7fe6beb8474a99a3e7eeb8c0f5da2ee8
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2025-05-15 17:09:53 +0530

    Java:SpringBoot:DanVega:dependancy_injection_spring:initialize

---
+++ Courses/Java/Spring/dan_vega_dependancy_injection/src/test/java/com/example/dan_vega_dependancy_injection/DanVegaDependancyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dan_vega_dependancy_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanVegaDependancyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
